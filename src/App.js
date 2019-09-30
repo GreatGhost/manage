8f35d33a31be2660836bf48f050a4f2007ff763b
GreatGhost <1609401009@qq.com>
2019-09-30 17:50:07 +0800

    后台管理

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from "antd";
-import "antd/dist/antd.css";
+// import "antd/dist/antd.css";
 
 
 function App() {
